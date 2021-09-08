5abaca44c3456e494bbe251d2e4397dc713f5fa1
Daniil-Solo <daniil.solo1723@gmail.com>
2021-09-08 20:42:35 +0300

    add lab1 - tasks: 1, 2

---
+++ Lab_1/task1.cs
@@ -0,0 +1,135 @@
+﻿// Лабораторная 1. Задание 1
+// Дана матрица из вещественных чисел, содержащая m строк и n столбцов.
+// Необходимо найти номер столбца матрицы, сумма элементов которого минимальна, применяя потоки
+// Реализовать создание строк матрицы параллельным способом
+
+using System;
+using System.Threading;
+
+namespace Lab1_Task1
+{
+    class Program
+    {
+        const int m = 10; // количество строк
+        const int n = 10; // количество столбцов
+
+        static void Main(string[] args)
+        {
+            float[][] matrix = new float[m][]; // матрица для хранения чисел
+            Thread[] threads = new Thread[m]; // массив для хранения потоков
+
+            // Создаем потоки для выделения памяти под строки матрицы
+            for (int i = 0; i < m; i++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(CreateRow);
+                Thread thread = new Thread(start);
+                thread.Name = "Поток №" + i.ToString();
+                threads[i] = thread;
+            }
+
+            // Запускаем потоки
+            for (int i=0; i<m; i++)
+            {
+                threads[i].Start(new Argument(matrix, i));
+            }
+
+            // Ждем завершения каждого потока
+            for (int i = 0; i < m; i++)
+            {
+                threads[i].Join();
+            }
+
+            // Заполняем матрицу случайными значениями от 0 до 2
+            Random random = new Random(12345);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i][j] = (byte)random.Next()%3;
+                    Console.Write(Convert.ToString(matrix[i][j]) + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            // Создаем массив с результатами суммирования
+            float[] sums = new float[n];
+
+            // Создаем потоки для подсчета суммы в каждом столбце
+            threads = new Thread[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(SumElementInColumn);
+                Thread thread = new Thread(start);
+                thread.Name = "Поток №" + i.ToString();
+                threads[i] = thread;
+            }
+
+            // Запускаем потоки
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Start(new Argument2(matrix, i, sums));
+            }
+
+            // Ждем завершения каждого потока
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Join();
+            }
+
+            // Определяем номер столбца с минимальной суммой элементов
+            int index_min_sum = 0;
+            float min_sum = float.MaxValue;
+            for(int i=0; i<n; i++)
+            {
+                if (min_sum > sums[i])
+                {
+                    min_sum = sums[i];
+                    index_min_sum = i;
+                }
+            }
+            Console.WriteLine("Столбец с наименьшей суммой элементов под номером " + index_min_sum.ToString() + ", с суммой равной " + min_sum.ToString());
+        }
+
+        public static void CreateRow(object argument)
+        {
+            Argument arg = (Argument)argument;
+            arg.matrix[arg.index] = new float[n];
+            string id = Thread.CurrentThread.Name;
+            Console.WriteLine(Convert.ToString(id));
+        }
+
+        public static void SumElementInColumn(object argument)
+        {
+            Argument2 arg = (Argument2)argument;
+            float sum = 0;
+            for (int i = 0; i < m; i++)
+                sum += arg.matrix[i][arg.index];
+            arg.sums[arg.index] = sum;
+            string id = Thread.CurrentThread.Name;
+            Console.WriteLine(Convert.ToString(id));
+        }
+
+    }
+
+    class Argument
+    {
+        public float[][] matrix;
+        public int index;
+
+        public Argument(float[][] matrix, int index)
+        {
+            this.matrix = matrix;
+            this.index = index;
+        }
+    }
+    class Argument2:Argument
+    {
+        public float[] sums;
+
+        public Argument2(float[][] matrix, int index, float[] sums): base(matrix, index)
+        {
+            this.sums = sums;
+        }
+    }
+}
---
+++ Lab_1/task2.cs
@@ -0,0 +1,136 @@
+﻿// Лабораторная 1. Задание 2
+// Дана матрица из вещественных чисел, содержащая n строк и m столбцов.
+// Отсортируйте каждую строку матрицы по убыванию методом подсчёта.
+
+using System;
+using System.Threading;
+
+namespace Lab1_Task1
+{
+    class Program
+    {
+        const int n = 10;// количество строк
+        const int m = 10; // количество столбцов
+        const byte max_value = 100; // максимальное значение элементам массива
+
+        static void Main(string[] args)
+        {
+            byte[][] matrix = new byte[n][]; // матрица для хранения чисел
+            Thread[] threads = new Thread[n]; // массив для хранения потоков
+
+            // Создаем потоки для выделения памяти под строки матрицы
+            for (int i = 0; i < n; i++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(CreateRow);
+                Thread thread = new Thread(start);
+                thread.Name = "Поток №" + i.ToString();
+                threads[i] = thread;
+            }
+
+            // Запускаем потоки
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Start(new Argument(matrix, i));
+            }
+
+            // Ждем завершения каждого потока
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Join();
+            }
+
+            // Заполняем матрицу случайными значениями от 0 до 2
+            Random random = new Random(12345);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i][j] = (byte)(random.Next() % (max_value+1));
+                    Console.Write(Convert.ToString(matrix[i][j]) + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------Сортировка-------------------------------------");
+            Console.WriteLine();
+
+            // Создаем потоки для сортировки
+            for (int i = 0; i < n; i++)
+            {
+                ParameterizedThreadStart start = new ParameterizedThreadStart(CountSort);
+                Thread thread = new Thread(start);
+                thread.Name = "Поток №" + i.ToString();
+                threads[i] = thread;
+            }
+
+            // Запускаем потоки
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Start(new Argument(matrix, i));
+            }
+
+            // Ждем завершения каждого потока
+            for (int i = 0; i < n; i++)
+            {
+                threads[i].Join();
+            }
+
+            // Вывод отсортированной матрицы
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(Convert.ToString(matrix[i][j]) + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void CreateRow(object argument)
+        {
+            string id = Thread.CurrentThread.Name;
+            Console.WriteLine(Convert.ToString(id) + " запустился");
+            Argument arg = (Argument)argument;
+            arg.matrix[arg.index] = new byte[n];
+            Console.WriteLine(Convert.ToString(id) + " завершился");
+        }
+
+        public static void CountSort(object argument)
+        {
+            string id = Thread.CurrentThread.Name;
+            Console.WriteLine(Convert.ToString(id) + " запустился");
+
+            Argument arg = (Argument)argument;
+            byte[] array = arg.matrix[arg.index];
+
+            byte[] c = new byte[max_value + 1];
+            for (int i = 0; i < c.Length; i++)
+                c[i] = 0;
+
+            for (int i = 0; i < m; i++)
+                ++c[array[i]];
+
+            Console.WriteLine(Convert.ToString(id) + " заполнил массив счетчиков");
+
+            int b = 0;
+            for (byte i = 0; i < max_value + 1; ++i)
+                for (int j = 0; j < c[i]; ++j)
+                    array[b++] = i;
+
+            Console.WriteLine(Convert.ToString(id) + " завершился");
+        }
+
+    }
+
+    class Argument
+    {
+        public byte[][] matrix;
+        public int index;
+
+        public Argument(byte[][] matrix, int index)
+        {
+            this.matrix = matrix;
+            this.index = index;
+        }
+    }
+}
