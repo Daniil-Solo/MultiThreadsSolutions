53ba8df4f968f574774e08c484aead20d6f537f2
Daniil-Solo <daniil.solo1723@gmail.com>
2021-09-09 22:06:52 +0300

    add lab1(task4)

---
+++ Lab_1/task4.cs
@@ -0,0 +1,116 @@
+﻿// Лабораторная 1. Задание 4
+// Программа выводит на форму круги трёх разных цветов в случайных местах (три разных потока).
+// Необходимо предусмотреть возможность изменить приоритет потоков управляющими элементами на форме.
+
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Lab1_Task4
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        int radius = 50;
+        object locker = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 1;
+            comboBox3.SelectedIndex = 2;
+            button1.Focus();
+            graphics = pictureBox1.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((comboBox1.Text == comboBox2.Text) || (comboBox1.Text == comboBox3.Text) || (comboBox3.Text == comboBox2.Text))
+            {
+                graphics.Clear(Color.White);
+                return;
+            }
+            else
+            {
+                int w = pictureBox1.Size.Width;
+                int h = pictureBox1.Size.Height;
+                graphics.Clear(Color.White);
+
+                // Создаем потоки 
+                Thread[] threads = new Thread[3]; 
+                for (int i = 0; i < 3; i++)
+                {
+                    ParameterizedThreadStart start = new ParameterizedThreadStart(CreateCircle);
+                    Thread thread = new Thread(start);
+                    threads[i] = thread;
+                }
+
+                // Выставляем приоритеты
+                threads[0].Priority = ThreadPriority.Highest;
+                threads[1].Priority = ThreadPriority.Normal;
+                threads[2].Priority = ThreadPriority.Lowest;
+
+                // Запускаем потоки
+                threads[0].Start(new Argument(w, h, radius, GetColor(comboBox1.Text), graphics, locker));
+                threads[1].Start(new Argument(w, h, radius, GetColor(comboBox2.Text), graphics, locker));
+                threads[2].Start(new Argument(w, h, radius, GetColor(comboBox3.Text), graphics, locker));
+
+                // Ждем завершения каждого потока
+                for (int i = 0; i < 3; i++)
+                {
+                    threads[i].Join();
+                }
+            }
+            
+        }
+
+        public static void CreateCircle(object argument)
+        {
+            Argument arg = (Argument)argument;
+
+            Random random = new Random();
+            int x0 = 50 + random.Next() % (arg.w-100);
+            int y0 = 50 + random.Next() % (arg.h-100);
+            for(int ang=10; ang<231; ang+=10)
+            {
+                lock (arg.locker)
+                {
+                    arg.graphics.DrawArc(new Pen(arg.color, 3f), x0, y0, arg.r, arg.r, (float)ang, (float)(ang + 10));
+                    Thread.Sleep(50);
+                }
+            }
+
+        }
+
+        public static Color GetColor(string colorText)
+        {
+            if (colorText == "Красный")
+                return Color.Red;
+            else if (colorText == "Синий")
+                return Color.Blue;
+            else
+                return Color.Green;
+        }
+    }
+    class Argument
+    {
+        public int w;
+        public int h;
+        public int r;
+        public Color color;
+        public Graphics graphics;
+        public object locker;
+
+        public Argument(int w, int h, int r, Color color, Graphics graphics, object locker)
+        {
+            this.w = w;
+            this.h = h;
+            this.r = r;
+            this.color = color;
+            this.graphics = graphics;
+            this.locker = locker;
+        }
+    }
+}
---
+++ Lab_1/task4_designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace Lab1_Task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 323);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Красный",
+            "Зеленый",
+            "Синий"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 26);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(599, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(165, 65);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Первый";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(599, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(165, 65);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Второй";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Красный",
+            "Зеленый",
+            "Синий"});
+            this.comboBox2.Location = new System.Drawing.Point(6, 26);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox3);
+            this.groupBox3.Location = new System.Drawing.Point(599, 211);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(165, 65);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Третий";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Красный",
+            "Зеленый",
+            "Синий"});
+            this.comboBox3.Location = new System.Drawing.Point(6, 26);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 379);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
